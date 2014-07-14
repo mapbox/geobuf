ad278cfcd363fd3593e36c387e2bd9cb8ae5241a
Tom MacWright <tom@macwright.org>
2014-07-14 18:13:14 -0400

    Decoding

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var ProtoBuf = require('protobufjs'),
     assert = require('assert'),
+    _ = require('underscore'),
     fs = require('fs');
 
 module.exports.builder = builder;
 module.exports.featureToGeobuf = featureToGeobuf;
+module.exports.geobufToFeature = geobufToFeature;
 
 function builder() {
     var b = ProtoBuf.loadProtoFile(__dirname + '/geobuf.proto');
     return b;
 }
+
+var geotypeMap = {
+    POINT: 'Point',
+    MULTIPOINT: 'MultiPoint',
+    LINESTRING: 'LineString',
+    MULTILINESTRING: 'MultiLineString',
+    POLYGON: 'Polygon',
+    MULTIPOLYGON: 'MultiPolygon',
+};
+
+var geotypeMapRev = _.invert(geotypeMap);
 
 function featureToGeobuf(geojson) {
 
@@ -20,10 +33,10 @@ function featureToGeobuf(geojson) {
     var geometry = new (b.build('geometry'))();
     var geometryTypes = b.build('geometry.Type');
 
-    if (geometryTypes[geojson.geometry.type.toUpperCase()] === undefined) {
+    if (geometryTypes[geotypeMapRev[geojson.geometry.type]] === undefined) {
         assert.fail('geometry type unknown');
     }
-    geometry.type = geometryTypes[geojson.geometry.type.toUpperCase()];
+    geometry.type = geometryTypes[geotypeMapRev[geojson.geometry.type]];
 
     var coordArray = new (b.build('coord_array'))();
     if (geojson.geometry.type === 'Point') {
@@ -38,15 +51,57 @@ function featureToGeobuf(geojson) {
         var p = new propBuild(),
             val = new valueBuild(),
             v = geojson.properties[k];
+
         p.set('key', k);
-        if (typeof v === 'string') {
-            val.set('string_value',  v);
+        switch (typeof v) {
+            case 'number':
+                val.set('float_value',  v);
+                break;
+            case 'boolean':
+                val.set('bool_value',  v);
+                break;
+            case 'string':
+                val.set('string_value',  (v || '').toString());
+                break;
+            default:
+                val.set('string_value',  (v || '').toString());
+                break;
         }
+
         p.set('value', val);
         feature.add('properties', p);
     }
 
     geometry.add('coord_array', coordArray);
 
+    feature.add('geometries', geometry);
+
     return feature.encode();
+}
+
+function geobufToFeature(buf) {
+    var b = builder();
+    var Feature = b.build('feature');
+    var feature = Feature.decode(buf);
+    var geojson = {
+        type: 'Feature',
+        properties: {},
+        geometry: {}
+    };
+    for (var i = 0; i < feature.properties.length; i++) {
+        // inefficient!
+        geojson.properties[feature.properties[i].key] = _.find(
+            _.values(feature.properties[i].value), truthy);
+    }
+    function truthy(val) {
+        return val !== null;
+    }
+    var geometryTypes = _.invert(b.build('geometry.Type'));
+    geojson.geometry.type = geotypeMap[geometryTypes[feature.geometries[0].type]];
+    var arr = feature.geometries[0].coord_array[0];
+    geojson.geometry.coordinates = [];
+    for (i = 0; i < arr.coords.length; i++) {
+        geojson.geometry.coordinates.push(arr.coords[i].toNumber());
+    }
+    return geojson;
 }
--- test/validate.test.js
+++ test/validate.test.js
@@ -1,6 +1,18 @@
 var geobuf = require('../'),
     test = require('tap').test;
 
+var feat = {
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        coordinates: [0, 0]
+    },
+    properties: {
+        name: 'Hello world',
+        b: 2
+    }
+};
+
 test('proto', function(t) {
     t.doesNotThrow(function() {
         var builder = geobuf.builder();
@@ -10,18 +22,12 @@ test('proto', function(t) {
 })
 
 test('featureToGeobuf', function(t) {
-    var buf = geobuf.featureToGeobuf({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [0, 0]
-        },
-        properties: {
-            name: 'Hello world'
-        }
-    });
-
+    var buf = geobuf.featureToGeobuf(feat);
     t.ok(buf, 'encodes a message');
+    t.end();
+});
 
+test('geobufToFeature', function(t) {
+    t.deepEqual(geobuf.geobufToFeature(geobuf.featureToGeobuf(feat)), feat);
     t.end();
 });
