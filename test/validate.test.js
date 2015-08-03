a9aad93a701670264fcedaca2a994a98b50cbf1a
Ryan Clark <ryan.clark.j@gmail.com>
2015-08-03 09:58:04 -0400

    concatenation test

--- test/validate.test.js
+++ test/validate.test.js
@@ -5,6 +5,8 @@ var geobuf = require('../'),
     Pbf = require('pbf'),
     test = require('tap').test,
     fs = require('fs'),
+    os = require('os'),
+    crypto = require('crypto'),
     path = require('path');
 
 for (var name in geojsonFixtures) {
@@ -21,6 +23,28 @@ test('roundtrip valid closed polygon with high-precision coordinates', function 
     var ring = geobuf.decode(pbf).features[0].geometry.coordinates[0];
     t.same(ring[0], ring[4]);
     t.end();
+});
+
+test('concatenation', function (t) {
+    var output = path.join(os.tmpdir(), crypto.randomBytes(8).toString('hex'));
+    var outStream = fs.createWriteStream(output);
+    var props = fs.createReadStream(path.join(__dirname, 'fixtures', 'props.pbf'));
+    var propsJson = fs.readFileSync(path.join(__dirname, 'fixtures', 'props.json'), 'utf8');
+    propsJson = JSON.parse(propsJson);
+    var multi = fs.createReadStream(path.join(__dirname, 'fixtures', 'single-multipoly.pbf'));
+    var multiJson = fs.readFileSync(path.join(__dirname, 'fixtures', 'single-multipoly.json'), 'utf8');
+    multiJson = JSON.parse(multiJson);
+
+    props.on('end', function () {
+        multi.pipe(outStream).on('finish', checkResult);
+    }).pipe(outStream, {end: false});
+
+    function checkResult() {
+        var buf = fs.readFileSync(output);
+        var result = geobuf.decode(new Pbf(buf));
+        t.deepEqual(result, [propsJson, multiJson]);
+        t.end();
+    }
 });
 
 function roundtripTest(geojson) {
