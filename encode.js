4a3c50d95b8f9a79e54f3b2b5bcbc3b5fe04d949
James Bursa <james@zamez.org>
2015-12-14 14:47:18 -0500

    Fix accumulating precision errors in delta encoding.
    Features with high-precision coordinates and lines or polygons containing many points can accumulate an error due to the delta encoding method, which only considers the previous point when computing the delta:
    
      delta[n] = x[n] - x[n - 1]
    
    Instead use the sum of all the previous deltas:
    
      delta[n] = x[n] - sum(delta[0 .. n-1])
    
    This prevents rounding errors from accumulating. A cumulative sum is kept for speed instead of recomputing the sum each time.

--- encode.js
+++ encode.js
@@ -213,8 +213,14 @@ function writeMultiPolygon(polygons, pbf) {
 
 function populateLine(coords, line, closed) {
     var i, j,
-        len = line.length - (closed ? 1 : 0);
+        len = line.length - (closed ? 1 : 0),
+        sum = new Array(dim);
+    for (j = 0; j < dim; j++) sum[j] = 0;
     for (i = 0; i < len; i++) {
-        for (j = 0; j < dim; j++) coords.push(Math.round((line[i][j] - (i ? line[i - 1][j] : 0)) * e));
+        for (j = 0; j < dim; j++) {
+            var n = Math.round(line[i][j] * e) - sum[j];
+            coords.push(n);
+            sum[j] += n;
+        }
     }
 }
